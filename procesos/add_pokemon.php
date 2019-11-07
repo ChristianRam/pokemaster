ec94bee5646532338e4ef866ba1f60c713861e69
Steven <christianram19@hotmail.com>
2019-11-07 06:29:00 -0500

    Corrigiendo errores

--- procesos/add_pokemon.php
+++ procesos/add_pokemon.php
@@ -2,7 +2,7 @@
 try{
 	include('../conexion/conexion.php');
     
-	$sql = $base_de_datos->prepare("INSERT INTO `entrenador_pokemones` (`id`, `id_entrenador`, `id_pokemon`) VALUES (NULL, ".$_GET['id_entrenador'].", ".$_GET['id_pokemon'].");");
+	$sql = $base_de_datos->prepare("INSERT INTO `entrenador_pokemones` (`id_entrenador_pokemones`, `id_entrenador`, `id_pokemon`) VALUES (NULL, ".$_GET['id_entrenador'].", ".$_GET['id_pokemon'].");");
 	//INSERT INTO `entrenador_pokemones` (`id`, `id_entrenador`, `id_pokemon`) VALUES (NULL, ".$_POST['id_entrenador'].", ".$_POST['id_pokemon'].");
 	//"UPDATE `entrenador` SET `id`=".$_POST['id'].", `nombre` = '".$_POST['nombre']."', `apellido` = '".$_POST['apellido']."', `alias` = '".$_POST['alias']."', id_region = ".$_POST['region'].", `descripcion` = '".$_POST['descripcion']."', `experiencia` = ".$_POST['experiencia'].", id_distincion = ".$_POST['distincion']."  WHERE `entrenador`.`id` = ".$_POST['id']."; "
     $sql->execute();
--- procesos/drop_entrenador.php
+++ procesos/drop_entrenador.php
@@ -1,7 +1,10 @@
 <?php
 try{
 	include('../conexion/conexion.php');
-    
+	
+	$sql1 = $base_de_datos->prepare("DELETE FROM `entrenador_pokemones` WHERE `entrenador_pokemones`.`id_entrenador` = ".$_GET['id']."; ");
+	$sql1->execute();
+
 	$sql = $base_de_datos->prepare("DELETE FROM `entrenador` WHERE `entrenador`.`id` = ".$_GET['id']."; ");
     
     $sql->execute();
